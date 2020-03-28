eaefcf138c37f08371d7d64a93a5e774becc2b68
levimoore1992 <levi.moore@exxonmobil.com>
2020-03-28 18:54:03 -0500

    fixed some issues

--- ImageFrontend/src/app/app.component.ts
+++ ImageFrontend/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ImageFrontend';
+  files: any;
+
+  onSelect($event: NgxDropzoneChangeEvent) {
+    
+  }
+
+  onRemove(f: any) {
+    
+  }
 }
