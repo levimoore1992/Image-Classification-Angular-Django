4caf34c6c3e88adca6f9022fc6e07d35cb8c48ba
levimoore1992 <levi.moore@exxonmobil.com>
2020-03-29 13:53:43 -0500

    Update app-routing.module.ts

--- ImageFrontend/src/app/app-routing.module.ts
+++ ImageFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {ImageListComponent} from "./image-list/image-list.component";
+import {HomeComponent} from './home/home.component';
+import {ImageListComponent} from './image-list/image-list.component';
 
 
 const routes: Routes = [
